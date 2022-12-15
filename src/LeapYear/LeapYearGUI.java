27b63e183ac7b8c1024e956b7639ad5f0f13ee8e
VennD0 <delapenavenndominic@gmail.com>
2022-12-15 14:52:25 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,8 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
@@ -8,6 +10,37 @@ public class LeapYearGUI extends JFrame{
     private JButton btnCheckYear;
 
     public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(500,500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Leap Year Checker");
+    }
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                is_LeapYear();
+            }
+        });
+    }
+
+    public int get_Year() {
+        String str = this.tfYear.getText();
+        return Integer.parseInt(str);
+    }
+    public void is_LeapYear() {
+
+            int year = get_Year();
+
+            if( year % 4 == 0){
+                JOptionPane.showMessageDialog(null, "Leap year");
+                tfYear.setText("");
+            } else {
+                JOptionPane.showMessageDialog(null, "Not a leap year");
+                tfYear.setText("");
+            }
     }
 }
